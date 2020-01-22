f16dc6fb196cec2062e67bbd135e52d81ade9bf9
Charles Pelton <charliejp0311@gmail.com>
2020-01-22 16:36:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,43 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  counter = 0 
+  attendees.each do |person|
+    attendees[counter] = badge_maker(person)
+    counter += 1
+  end 
+end 
+
+def assign_rooms(attendees)
+  counter = 0 
+  attendees.each do |person|
+    attendees[counter] = "Hello, #{person}! You'll be assigned to room #{counter + 1}!"
+    counter += 1 
+  end 
+end
+
+def printer(attendees)
+  badges = attendees
+  badges = batch_badge_creator(badges)
+  badges.each do |badge| 
+    pp badge 
+  end
+  romm_assignments = attendees
+  romm_assignments = assign_rooms(romm_assignments)
+  romm_assignments.each do |room| 
+    pp room 
+  end
+  b = 0 
+  #while b < attendees.length do
+  #  pp badges[b]
+  #  b += 1 
+  #end
+  #ra = 0 
+  #while ra < attendees.length do
+  #  pp romm_assignments[ra]
+  #  ra += 1
+  #end 
+end
